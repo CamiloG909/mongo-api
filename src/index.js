e16743d323c0f9abe51a7711f1803e978a49c41f
Camilo Galindo R <cagalindo35@misena.edu.co>
2022-10-02 11:12:19 -0500

    fix host

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ require("./database");
 
 app.set("port", process.env.PORT || 4000);
 
-app.listen(app.get("port"), () => {
+app.listen(app.get("port"), "0.0.0.0", () => {
   console.log("Server on port 4000");
 });
